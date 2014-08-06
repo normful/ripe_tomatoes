7524976e8b7cbb52aec1e4d8fc5f29804e398a57
Norman Sue <normful@gmail.com>
2014-08-06 11:35:32 -0700

    add sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
 
   root 'movies#index'
 
