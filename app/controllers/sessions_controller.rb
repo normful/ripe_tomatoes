f47e3781e9cf8a4cde19cd7c7d18cb95764083af
Norman Sue <normful@gmail.com>
2014-08-06 11:39:05 -0700

    add step 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to movies_path
+    else
+      render :new
+    end
   end
 end
