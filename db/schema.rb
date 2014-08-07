e9df94334c3f9a361fa0bc83190683544a9b1518
Norman Sue <normful@gmail.com>
2014-08-07 14:59:20 -0700

    add admin flag to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140806190603) do
+ActiveRecord::Schema.define(version: 20140807215842) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20140806190603) do
     t.datetime "updated_at"
     t.string   "firstname"
     t.string   "lastname"
+    t.boolean  "admin"
   end
 
 end
