6874dbdb7ff0dd1aac7f7f457ea76642ba182655
Norman Sue <normful@gmail.com>
2014-08-07 19:21:09 -0700

    admin user edit feature

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -26,9 +26,16 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
