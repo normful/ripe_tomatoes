ad938caa13c58ef5c2c90a745cbbe678aaf7d3ef
Norman Sue <normful@gmail.com>
2014-08-07 18:28:32 -0700

    add admin show user view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,16 +8,24 @@ class Admin::UsersController < ApplicationController
     @users = User.page(params[:page])
   end
 
-  def create
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users, notice: "#{@user.full_name} was successfully created."
+    else
+      render :new
+    end
   end
 
   def edit
-  end
-
-  def show
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
