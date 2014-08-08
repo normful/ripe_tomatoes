02c9141d06384e71ad080b63ee59e557405652df
Norman Sue <normful@gmail.com>
2014-08-07 19:41:37 -0700

    add delete user feature

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -39,6 +39,9 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  paginates_per 2
+  paginates_per 10
 
   def full_name
     "#{firstname} #{lastname}"
