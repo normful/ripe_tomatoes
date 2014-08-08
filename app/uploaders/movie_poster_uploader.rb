fbfa990dd5dbdf16c690c02ea9e34d5a8cb09664
Norman Sue <normful@gmail.com>
2014-08-07 17:16:30 -0700

    change default image URI

--- app/uploaders/movie_poster_uploader.rb
+++ app/uploaders/movie_poster_uploader.rb
@@ -20,8 +20,7 @@ class MoviePosterUploader < CarrierWave::Uploader::Base
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-
-    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+    "/default.jpg"
   end
 
   # Process files as they are uploaded:
